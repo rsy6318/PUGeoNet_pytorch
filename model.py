832654d11348285f31f5d4466a945220bc83cf12
rsy6318 <rsy6318@163.com>
2022-05-30 13:41:15 +0800

    Update model.py
    i. remove T-Net
    ii. use leakrelu to replace relu
    iii. remove dropout layers

--- model.py
+++ model.py
@@ -42,50 +42,6 @@ def get_graph_feature(x, k=20, idx=None):
     return feature
 
 
-class input_transform_net(nn.Module):
-    def __init__(self, input_dim, K=3):
-        super(input_transform_net, self).__init__()
-        self.K = K
-        self.tconv1 = nn.Sequential(nn.Conv2d(input_dim, 64, kernel_size=1),
-                                    nn.BatchNorm2d(64),
-                                    nn.ReLU())
-        self.tconv2 = nn.Sequential(nn.Conv2d(64, 128, kernel_size=1),
-                                    nn.BatchNorm2d(128),
-                                    nn.ReLU())
-        self.tconv3 = nn.Sequential(nn.Conv2d(128, 1024, kernel_size=1),
-                                    nn.BatchNorm2d(1024),
-                                    nn.ReLU())
-        self.tfc1 = nn.Sequential(nn.Linear(1024, 512),
-                                  nn.BatchNorm1d(512),
-                                  nn.ReLU())
-        self.tfc2 = nn.Sequential(nn.Linear(512, 256),
-                                  nn.BatchNorm1d(256),
-                                  nn.ReLU())
-
-    def forward(self, x):
-        # x:(B,C,N,K)
-        batchsize = x.size(0)
-        x = self.tconv1(x)
-        x = self.tconv2(x)
-        x = x.max(dim=-1, keepdim=True)[0]
-        x = self.tconv3(x)
-        x = x.max(dim=-2, keepdim=True)[0]
-        x = x.squeeze(-1).squeeze(-1)
-        x = self.tfc1(x)
-        x = self.tfc2(x)
-
-        weights = Variable(torch.from_numpy(np.zeros((256, self.K * self.K), dtype=np.float32)))
-        biases = Variable(torch.from_numpy(np.array([0, 0, 0, 0, 0, 0, 0, 0, 0]).astype(np.float32))).view(1,
-                                                                                                           self.K * self.K)
-        biases = biases + torch.eye(self.K).view(1, self.K * self.K)
-        weights = weights.to(x.device)
-        biases = biases.to(x.device)
-        transform = torch.matmul(x, weights)
-        transform = transform + biases
-        transform = transform.view(batchsize, self.K, self.K)
-        return transform
-
-
 class pugeonet(nn.Module):
     def __init__(self, up_ratio, knn=30, fd=64, fD=1024):
         super(pugeonet, self).__init__()
@@ -95,47 +51,47 @@ class pugeonet(nn.Module):
                           "out3", "net_max_2", "net_mean_2",
                           "out5", "net_max_3", "net_mean_3",
                           "out7", "out8"]
-        self.input_transform_net = input_transform_net(6)
+        #self.input_transform_net = input_transform_net(6)
 
         self.dgcnn_conv1 = nn.Sequential(nn.Conv2d(6, fd, kernel_size=1),
                                          nn.BatchNorm2d(fd),
-                                         nn.ReLU())
+                                         nn.LeakyReLU(negative_slope=0.2))
         self.dgcnn_conv2 = nn.Sequential(nn.Conv2d(fd, fd, kernel_size=1),
                                          nn.BatchNorm2d(fd),
-                                         nn.ReLU())
+                                         nn.LeakyReLU(negative_slope=0.2))
         self.dgcnn_conv3 = nn.Sequential(nn.Conv1d(fd + fd, fd, kernel_size=1),
                                          nn.BatchNorm1d(fd),
-                                         nn.ReLU())
+                                         nn.LeakyReLU(negative_slope=0.2))
         self.dgcnn_conv4 = nn.Sequential(nn.Conv2d(fd + fd, fd, kernel_size=1),
                                          nn.BatchNorm2d(fd),
-                                         nn.ReLU())
+                                         nn.LeakyReLU(negative_slope=0.2))
         self.dgcnn_conv5 = nn.Sequential(nn.Conv1d(fd + fd, fd, kernel_size=1),
                                          nn.BatchNorm1d(fd),
-                                         nn.ReLU())
+                                         nn.LeakyReLU(negative_slope=0.2))
         self.dgcnn_conv6 = nn.Sequential(nn.Conv2d(fd + fd, fd, kernel_size=1),
                                          nn.BatchNorm2d(fd),
-                                         nn.ReLU())
+                                         nn.LeakyReLU(negative_slope=0.2))
         self.dgcnn_conv7 = nn.Sequential(nn.Conv1d(fd + fd, fd, kernel_size=1),
                                          nn.BatchNorm1d(fd),
-                                         nn.ReLU())
+                                         nn.LeakyReLU(negative_slope=0.2))
         self.dgcnn_conv8 = nn.Sequential(nn.Conv1d(fd + fd + fd, fD, kernel_size=1),
                                          nn.BatchNorm1d(fD),
-                                         nn.ReLU())
+                                         nn.LeakyReLU(negative_slope=0.2))
 
         self.attention_conv1 = nn.Sequential(nn.Conv1d(fd * 9 + fD * 2, 128, kernel_size=1),
                                              nn.BatchNorm1d(128),
-                                             nn.ReLU())
+                                             nn.LeakyReLU(negative_slope=0.2))
         self.attention_conv2 = nn.Sequential(nn.Conv1d(128, 64, kernel_size=1),
                                              nn.BatchNorm1d(64),
-                                             nn.ReLU())
+                                             nn.LeakyReLU(negative_slope=0.2))
         self.attention_conv3 = nn.Sequential(nn.Conv1d(64, len(self.feat_list), kernel_size=1),
                                              nn.BatchNorm1d(len(self.feat_list)),
-                                             nn.ReLU())
+                                             nn.LeakyReLU(negative_slope=0.2))
 
         self.concat_conv = nn.Sequential(nn.Conv1d(fd * 9 + fD * 2, 256, kernel_size=1),
                                          nn.BatchNorm1d(256),
-                                         nn.ReLU())
-        self.dg1 = nn.Dropout(p=0.4)
+                                         nn.LeakyReLU(negative_slope=0.2))
+        self.dg1 = nn.Sequential()
 
         self.uv_conv1 = nn.Sequential(nn.Conv1d(256, up_ratio * 2, kernel_size=1))
 
@@ -143,28 +99,28 @@ class pugeonet(nn.Module):
 
         self.normal_offset_conv1 = nn.Sequential(nn.Conv1d(256, up_ratio * 3, kernel_size=1))
 
-        self.up_layer1 = nn.Sequential(nn.Conv1d(256 + 3, 128, kernel_size=1),
-                                       nn.BatchNorm1d(128),
-                                       nn.ReLU())
+        self.up_layer1 = nn.Sequential(nn.Conv2d(256 + 3, 128, kernel_size=1),
+                                       nn.BatchNorm2d(128),
+                                       nn.LeakyReLU(negative_slope=0.2))
 
-        self.up_dg1 = nn.Dropout(p=0.4)
-        self.up_layer2 = nn.Sequential(nn.Conv1d(128, 128, kernel_size=1),
-                                       nn.BatchNorm1d(128),
-                                       nn.ReLU())
-        self.up_dg2 = nn.Dropout(p=0.4)
+        self.up_dg1 = nn.Sequential()
+        self.up_layer2 = nn.Sequential(nn.Conv2d(128, 128, kernel_size=1),
+                                       nn.BatchNorm2d(128),
+                                       nn.LeakyReLU(negative_slope=0.2))
+        self.up_dg2 = nn.Sequential()
 
-        self.fc_layer = nn.Conv1d(128, 1, kernel_size=1)
+        self.fc_layer = nn.Conv2d(128, 1, kernel_size=1)
 
-    def forward(self, x, bradius=1.0):
+    def forward(self, x):
         # x:(B,3,N)
         batch_size = x.size(0)
         num_point = x.size(2)
         edge_feature = get_graph_feature(x, k=self.knn)
-        transform = self.input_transform_net(edge_feature)
+        '''transform = self.input_transform_net(edge_feature)
 
         point_cloud_transformed = torch.bmm(x.transpose(1, 2), transform)
         point_cloud_transformed = point_cloud_transformed.transpose(1, 2)
-        edge_feature = get_graph_feature(point_cloud_transformed, k=self.knn)
+        edge_feature = get_graph_feature(point_cloud_transformed, k=self.knn)'''
         out1 = self.dgcnn_conv1(edge_feature)
         out2 = self.dgcnn_conv2(out1)
         net_max_1 = out2.max(dim=-1, keepdim=False)[0]
@@ -237,8 +193,10 @@ class pugeonet(nn.Module):
         uv_3d = uv_3d.permute(0, 2, 3, 1)  # (B,U,3,N)
         uv_3d = x.unsqueeze(1).repeat(1, self.up_ratio, 1, 1) + uv_3d  # (B,U,3,N)
 
+        uv_3d = uv_3d.transpose(1,2)        #(B,3,U,N)
+
         # B,3,U,N
-        uv_3d = uv_3d.permute(0, 2, 1, 3).reshape(batch_size, 3, self.up_ratio * num_point)
+        #uv_3d = uv_3d.permute(0, 2, 1, 3).reshape(batch_size, 3, self.up_ratio * num_point)
 
         # norm predict
         dense_normal_offset = self.normal_offset_conv1(concat)
@@ -252,50 +210,31 @@ class pugeonet(nn.Module):
         sparse_normal = F.normalize(sparse_normal, dim=2)  # B,1,3,N
 
         dense_normal = sparse_normal.repeat(1, self.up_ratio, 1, 1, ) + dense_normal_offset
-        dense_normal = F.normalize(dense_normal, dim=2)  # B, UP, 3, N
+        dense_normal = F.normalize(dense_normal, dim=2)  # B, U, 3, N
 
-        dense_normal = dense_normal.permute(0, 2, 1, 3).reshape(batch_size, 3, self.up_ratio * num_point)
+        dense_normal=dense_normal.transpose(1,2).reshape(batch_size,3,-1)        #(B,U,3,N)->(B,3,U,N)->(B,3,U*N)
 
-        if not np.isscalar(bradius):
-            bradius_expand = bradius.unsqueeze(-1).unsqueeze(-1)
-        else:
-            bradius_expand = bradius
-        grid = uv_3d * bradius_expand
+        grid = uv_3d        #(B,3,U,N)
 
-        concat_up = concat.repeat(1, 1, self.up_ratio)
-        concat_up = torch.cat((concat_up, grid), axis=1)
+        #concat_up = concat.repeat(1, 1, self.up_ratio)
+        concat_up=concat.unsqueeze(2).repeat(1,1,self.up_ratio,1)   #(B,C,U,N)
+        concat_up = torch.cat((concat_up, grid), axis=1)            #(B,C+3,U,N)
 
         concat_up = self.up_layer1(concat_up)
         concat_up = self.up_dg1(concat_up)
         concat_up = self.up_layer2(concat_up)
         concat_up = self.up_dg2(concat_up)
 
-        coord_z = self.fc_layer(concat_up)  # B,1,U*N
-
-        coord_z = coord_z.reshape(batch_size, 1, self.up_ratio, num_point)  # B,1,U,N
-
-        coord_z = coord_z.permute(0, 1, 3, 2)  # B,1,N,U
-
-        coord_z = torch.cat((torch.zeros_like(coord_z), torch.zeros_like(coord_z), coord_z), dim=1)  # B,3,N,U
-
-        coord_z = torch.matmul(coord_z.permute(0, 2, 3, 1), affine_T.permute(0, 3, 1, 2))  # B,N,U,3
-
-        coord_z = coord_z.permute(0, 3, 2, 1).reshape(batch_size, 3, num_point * self.up_ratio)
-
-        coord = uv_3d + coord_z
-        return coord, dense_normal, sparse_normal.squeeze(1)
+        coord_z = self.fc_layer(concat_up)  #(B,1,U,N)
 
 
-if __name__ == '__main__':
-    a = torch.rand(10, 3, 1024)
+        coord_z = torch.cat((torch.zeros_like(coord_z), torch.zeros_like(coord_z), coord_z), dim=1)  # B,3,U,N
 
-    net = pugeonet(5).cuda()
-    a = a.cuda()
-    b, c, d = net(a)
-    print(b.size())
-    print(c.size())
-    print(d.size())
-    '''a=torch.tensor([[1,2,3],[4,5,6]])
-    print(a)
-    a=a.repeat(1,2)
-    print(a)'''
+        coord_z = torch.matmul(coord_z.permute(0, 3, 2, 1), affine_T.permute(0, 3, 1, 2))  # B,N,U,3
+        
+        coord = uv_3d + coord_z.permute(0, 3, 2, 1)     #(B,3,U,N)
+
+        coord=coord.reshape(batch_size,3,-1)
+
+        return {'dense_xyz':coord, 'dense_normal':dense_normal, 'sparse_normal':sparse_normal.squeeze(1)}
+
